109c4463b6b0371b14a27fdfb8d437eb1793de06
abhishek47kashyap <abhishek47kashyap@gmail.com>
2020-03-21 04:09:27 +0900

    Renderer, Textures, Blending

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@
 #include "IndexBuffer.h"
 #include "VertexArray.h"
 #include "Shader.h"
+#include "Texture.h"
 
 int main() {
     GLFWwindow* window;
@@ -55,19 +56,27 @@ int main() {
     glBindVertexArray(vertexArrayID);
 
     // creating buffer for triangles vertices
-    float vertexCoordinates[] = {-0.5f, -0.5f,    // corner 0
-                                  0.5f, -0.5f,    // corner 1
-                                  0.5f,  0.5f,    // corner 2
-                                 -0.5f,  0.5f     // corner 3
+    float vertexCoordinates[] = {-0.5f, -0.5f,    // corner 0 (bottom left)
+                                                0.0f,  0.0f,    // texture coordinates for corner 0
+                                  0.5f, -0.5f,    // corner 1 (bottom right)
+                                                1.0f,  0.0f,    // texture coordinates for corner 1
+                                  0.5f,  0.5f,    // corner 2 (top right)
+                                                1.0f,  1.0f,    // texture coordinates for corner 2
+                                 -0.5f,  0.5f,    // corner 3 (top left)
+                                                0.0f,  1.0f     // texture coordinates for corner 3
                                 };
 
     unsigned int indices[] = {0, 1, 2,
                               2, 3, 0};   // building a square out of vertexCoordinates
 
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
     VertexArray va;
-    VertexBuffer vb(vertexCoordinates, 4 * 2 * sizeof(float));
+    VertexBuffer vb(vertexCoordinates, 4 * 4 * sizeof(float));
 
     VertexBufferLayout layout;
+    layout.Push<float>(2);
     layout.Push<float>(2);
     va.AddBuffer(vb, layout);
 
@@ -82,26 +91,32 @@ int main() {
     // color animation
     float blueChannel = 0.0f, increment = 0.05;
 
+    // Textures
+    Texture texture("resources/textures/Avengers_logo.png");
+    texture.Bind();  // default slot is 0
+    shader.SetUniform1i("u_Texture", 0);   // 0 is slot number, which should be the same as texture.Bind()
+
     // Unbinding
     va.Unbind();
     vb.Unbind();   // same as glBindBuffer(GL_ARRAY_BUFFER, 0);
     ib.Unbind();   // same as glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
     shader.Unbind();
 
+    // Create instance of Renderer
+    Renderer renderer;
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+        renderer.Clear();
 
         // binding every iteration
         shader.Bind();
         shader.SetUniform4f("u_Color", 0.2f, 0.3f, blueChannel, 1.0f);
 
-        va.Bind();
-        ib.Bind();
-
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);   // Square
+        // render to screen
+        renderer.Draw(va, ib, shader);
 
         // changing blue channel intensity
         if (blueChannel > 1.0f || blueChannel < 0.0f)
