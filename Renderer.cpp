44422f19a17b109fd898cb4e9bc84b5c46676dd5
abhishek47kashyap <abhishek47kashyap@gmail.com>
2020-03-21 02:11:05 +0900

    Basic renderer

--- Renderer.cpp
+++ Renderer.cpp
@@ -17,3 +17,18 @@ bool GLLogCall()
     }
     return true;
 }
+
+
+void Renderer::Draw(const VertexArray &va, const IndexBuffer &ib, const Shader &shader) const
+{
+    shader.Bind();
+    va.Bind();
+    ib.Bind();
+
+    glDrawElements(GL_TRIANGLES, ib.GetCount(), GL_UNSIGNED_INT, nullptr);   // Square
+}
+
+void Renderer::Clear() const
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+}
--- Renderer.h
+++ Renderer.h
@@ -1,6 +1,9 @@
 
 #include <iostream>
 #include <GL/glew.h>
+#include "VertexArray.h"
+#include "IndexBuffer.h"
+#include "Shader.h"
 
 #ifndef CHERNO_OPENGL_RENDERER_H
 #define CHERNO_OPENGL_RENDERER_H
@@ -9,6 +12,14 @@
 void GLClearError();
 
 bool GLLogCall();
+
+
+class Renderer
+{
+public:
+    void Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader) const;
+    void Clear() const;
+};
 
 
 #endif //CHERNO_OPENGL_RENDERER_H
